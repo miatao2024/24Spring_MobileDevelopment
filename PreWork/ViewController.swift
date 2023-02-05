8ba628ab353838bcae6ea225b176de681c6237e2
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-02-05 13:28:41 -0800

    Initial Commit

---
+++ PreWork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PreWork
+//
+//  Created by Minjiaaa on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
