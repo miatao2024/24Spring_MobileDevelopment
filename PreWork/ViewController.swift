cd10a6b9e12aa55d2a5c3a19cab0da7e4d709c5b
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-02-05 17:54:19 -0800

    Added interest part in App

--- PreWork/ViewController.swift
+++ PreWork/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var numberOfPetsLabel: UILabel!
     
+    @IBOutlet weak var interestSegmentedControl: UISegmentedControl!
+    
+    
     @IBAction func stepperDidChange(_ sender: UIStepper) {
        // updates the text of a label whenever the value of the stepper is changed.
        numberOfPetsLabel.text = "\(Int(sender.value))"
@@ -31,8 +34,11 @@ class ViewController: UIViewController {
        // In our example that is whether it is first, second, third or forth
        let year = yearSegmentedControl.titleForSegment(at: yearSegmentedControl.selectedSegmentIndex)
        
+    // choose one of the interest that students want to learn at Codepath
+       let interest = interestSegmentedControl.titleForSegment(at: interestSegmentedControl.selectedSegmentIndex)
+        
        // Creating a constant of type string that holds an introduction. The introduction receives the values from the outlet connections.
-       let introduction = "My name is \(FirstNameTextField.text!) \(LastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets."
+       let introduction = "My name is \(FirstNameTextField.text!) \(LastNameTextField.text!) and I attend \(schoolNameTextField.text!). I am currently in my \(year!) year and I own \(numberOfPetsLabel.text!) dogs. It is \(morePetsSwitch.isOn) that I want more pets. My interest lies in \(interest!). I hope to learn more at Codepath."
         
         
     // Creates the alert where we pass in our message, which our introduction.
