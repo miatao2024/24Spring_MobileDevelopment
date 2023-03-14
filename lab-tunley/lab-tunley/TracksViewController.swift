de0e61d433808229964484cf689ddb218b8d36e1
tao.mi <94973185+Minjiaaa@users.noreply.github.com>
2023-03-13 23:42:02 -0700

    lab2

---
+++ lab-tunley/lab-tunley/TracksViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  lab-tunley
+//
+//  Created by Charlie Hieger on 12/1/22.
+//
+
+import UIKit
+
+class TracksViewController: UIViewController { // 用来干嘛的？
+
+    // TODO: Pt 1 - Add a tracks property
+    var tracks: [Track] = []
+
+
+    // TODO: Pt 1 - Add table view outlet
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // TODO: Pt 1 - Set tracks property with mock tracks array
+        tracks = Track.mockTracks
+        print(tracks)
+
+        
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // TODO: Pt 1 - Pass the selected track to the detail view controller
+
+
+    }
+
+    // TODO: Pt 1 - Add table view data source methods
+
+
+}
